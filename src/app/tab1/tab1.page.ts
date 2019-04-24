0132ed98147aea446b5515734d84a79eb10842bd
Maremo1 <maremo.manthata@gmail.com>
2019-04-24 18:54:54 +0200

    push with observable error

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { RestService } from './../services/rest.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  tanks: Observable<any>;
+ 
+  constructor(public restService: RestService) { 
+    this.tanks = this.restService.getSystemData();
+  }
+ 
+  
+}
